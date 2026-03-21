9a670ccbaf532f6319e4097dc04c5a4cee0e039a
Startingfindmistakesamsung <dudgh2328@gmail.com>
2026-03-21 16:46:54 +0900

    fold name fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,7 +64,7 @@ export default defineConfig({
 					autogenerate: { directory: 'machine_learning' },
 				},
 				{
-					label: 'Markdown',
+					label: 'Markdowns',
 					autogenerate: { directory: 'markdown' },
 				},
 
